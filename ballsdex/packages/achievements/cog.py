685b5d56cc65e92b4164c73283ec8258b6453c58
nepthune <131011964+nepthunium@users.noreply.github.com>
2024-09-15 13:10:58 -0700

    UPDATE COG.PY

--- ballsdex/packages/achievements/cog.py
+++ ballsdex/packages/achievements/cog.py
@@ -42,6 +42,8 @@ class Achievements(commands.GroupCog, group_name="achievements"):
     def __init__(self, bot: "BallsDexBot"):
         self.bot = bot
 
+    rewards = app_commands.Group(name="rewards", description="Manage rewards")
+
     @app_commands.command()
     async def list(self, interaction: discord.Interaction):
         """
@@ -113,9 +115,9 @@ class Achievements(commands.GroupCog, group_name="achievements"):
 
                 if has_a == False:
                     rewards = str(a.rewards).split(";")
-
                     if rewards:
                         for r in rewards:
+                            shiny = False
                             if r.startswith("✨ "):
                                 split_r = r[2:].split()
                                 if len(split_r) > 0:
@@ -124,18 +126,12 @@ class Achievements(commands.GroupCog, group_name="achievements"):
                                 else:
                                     continue
                             else:
-                                split_r = r.split()
-                                if len(split_r) > 0:
-                                    reward = split_r[0]
-                                    shiny = False
-                                else:
-                                    continue
+                                reward = r.strip()
                             try:
                                 b = await Ball.get(country=reward)
                                 await BallInstance.create(ball=b, player=player, shiny=shiny)
                             except DoesNotExist:
                                 continue
-
                     await AchievementInstance.create(achievement=a, player=player)
                     message.append(f"Found and gave missing achievement: **{a.name}**\n")
         
@@ -143,3 +139,42 @@ class Achievements(commands.GroupCog, group_name="achievements"):
             await interaction.followup.send("\n".join(message), ephemeral=True)
         else:
             await interaction.followup.send("No new achievements found.", ephemeral=True)
+        
+    @rewards.command(name="list")
+    async def rewards_list(self, interaction: discord.Interaction):
+        """
+        Displays the list of achievements and their rewards
+        """
+        achievements = await Achievement.filter(achievable=True)
+        entries = []
+        for a in achievements:
+            if a.rewards:
+                rewards = str(a.rewards).split(';')
+                reward_count = {}
+                for reward in rewards:
+                    cleaned_reward = reward.strip()
+                    if cleaned_reward:
+                        if cleaned_reward in reward_count:
+                            reward_count[cleaned_reward] += 1
+                        else:
+                            reward_count[cleaned_reward] = 1
+
+                rewards = '\n'.join(f"- {count}x {reward}" if count > 1 else f"- {reward}" 
+                                    for reward, count in reward_count.items())
+            else:
+                rewards = "- No rewards for this achievement."
+            
+            entries.append((f"**{a.name}:**", f"Rewards:\n{rewards}"))
+
+        if len(entries) == 0:
+            await interaction.response.send_message("There are no achievements registered on this bot.", ephemeral=True)
+            return
+        
+        avatar_url = interaction.user.avatar.url if interaction.user.avatar else DEFAULT_AVATAR_URL
+
+        source = FieldPageSource(entries, per_page=4, inline=False, clear_description=False)
+        source.embed.description = "**List of Achievements' Rewards**"
+        source.embed.colour = discord.Colour.blurple()
+        source.embed.set_thumbnail(url=avatar_url)
+        pages = Pages(source=source, interaction=interaction, compact=True)
+        await pages.start()
